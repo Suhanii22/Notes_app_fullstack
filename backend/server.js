2aa61bdf9a96a143495190600b780cf894e68c96
Suhanii <patilsuhani14@gmail.com>
2025-09-28 19:52:20 +0530

    corrected the url of frontend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const cors = require('cors');
 
-const allowedOrigins = ['http://localhost:5173', 'https://visionary-caramel-0f75f4.netlify.app']; 
+const allowedOrigins = ['http://localhost:5173', 'https://notes-app-fullstack-m18kp6fts-suhanii22s-projects.vercel.app/']; 
 
 app.use(cors({
   origin: function(origin, callback) {
